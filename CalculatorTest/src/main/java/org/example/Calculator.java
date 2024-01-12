930e9961edc934930ba5bb4f920b521315048906
baekseungchan1117 <bsc7386@gmail.com>
2024-01-12 16:58:47 +0900

    calculate interface

--- CalculatorTest/src/main/java/org/example/Calculator.java
+++ CalculatorTest/src/main/java/org/example/Calculator.java
@@ -1,8 +1,16 @@
 package org.example;
 
-public class Calculator {
+import org.example.calculate.*;
 
+import java.util.List;
+
+public class Calculator {
+    private static final List<NewArithmeticOperator> arithmeticOperators = List.of(new AdditionOperator(), new SubtractionOperator(), new MultiplicationOperator(), new DivisionOperator());
     public static int calculate(int opperand1, String opperator, int opperand2){
-        return ArithmeticOperator.calculate(opperand1, opperator, opperand2);
+        return arithmeticOperators.stream()
+                .filter(arithmeticOperators -> arithmeticOperators.supports(opperator))
+                .map(arithmeticOperators -> arithmeticOperators.calculate(opperand1, opperand2))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("올바른 사칙연산이 아닙니다."));
     }
 }
